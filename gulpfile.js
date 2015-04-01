04b8bb3982b0ef19af122ad46b4618bb3e3849bb
Rob Gerstenberger <rgerstenberger@walmartlabs.com>
2015-04-01 13:16:01 -0700

    add test infrastructure

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var react = require('gulp-react');
 var rimraf = require('gulp-rimraf');
 var eslint = require('gulp-eslint');
+var karma = require('karma').server;
 
 gulp.task('lint', function () {
   return gulp.src(['src/**/*.jsx'])
@@ -21,6 +22,13 @@ gulp.task('jsx-compile', ['clean-lib'], function () {
 
 gulp.task('watch', ['jsx-compile'], function () {
     gulp.watch('src/**/*.jsx', ['jsx-compile']);
+});
+
+gulp.task('test', ['jsx-compile'], function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  });
 });
 
 gulp.task('default', ['watch']);
---
+++ test/spec/select.spec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+describe('Button', function () {
+
+  var React = require('react');
+  var TestUtils = require('react/lib/ReactTestUtils');
+
+  var RadonSelect = require('select.jsx');
+  var container, component;
+
+  describe('Mounting', function() {
+
+    beforeEach(function() {
+      container = document.createElement('div');
+      component = React.render(
+        React.createElement(RadonSelect, {selectName: "test"}, [
+          React.createElement(RadonSelect.Option, {key: "blah"}, "blah"),
+          React.createElement(RadonSelect.Option, {key: "foo"}, "foo")
+        ]),
+        container
+      );
+    });
+
+    afterEach(function() {
+      React.unmountComponentAtNode(container);
+    });
+
+    it('should render into the document', function() {
+        expect(component.isMounted()).to.be.true;
+    });
+
+  });
+});
