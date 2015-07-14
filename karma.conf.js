4fe69f5a08ffd2af20b84339366c3fcad5bf8f2a
Alex Lande <alexlande@gmail.com>
2015-07-14 15:23:57 -0700

    Move test to npm scripts

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ var path = require('path');
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['mocha', 'sinon-chai'],
+    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     files: [
       'test/helpers/**/*.js',
       'test/spec/**/*.js'
@@ -16,35 +16,53 @@ module.exports = function (config) {
     webpack: {
       cache: true,
       module: {
+        preLoaders: [{
+          test: /\.jsx$/,
+          loader: 'babel-loader',
+          include: path.resolve('test')
+        }],
         loaders: [{
           test: /\.jsx$/,
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          exclude: [/node_modules/]
         }]
       },
       resolve: {
-        root: [path.join(__dirname, '/src')]
+        root: [path.join(__dirname, '/src')],
+        modulesDirectories: ['node_modules', 'modules'],
+        extensions: ['', '.js', '.jsx']
       }
     },
     webpackServer: {
+      quiet: false,
+      noInfo: true,
       stats: {
-        colors: true
+        assets: false,
+        colors: true,
+        version: false,
+        hash: false,
+        timings: false,
+        chunks: false,
+        chunkModules: false
       }
     },
     exclude: [],
     port: 8080,
     logLevel: config.LOG_INFO,
+    browserNoActivityTimeout: 60000,
     colors: true,
     autoWatch: false,
     browsers: ['PhantomJS'],
     reporters: ['mocha'],
     plugins: [
-      require('karma-mocha'),
-      require('karma-mocha-reporter'),
-      require('karma-phantomjs-launcher'),
-      require('karma-sinon-chai'),
-      require('karma-webpack')
+      'karma-mocha',
+      'karma-mocha-reporter',
+      'karma-phantomjs-shim',
+      'karma-phantomjs-launcher',
+      'karma-sinon-chai',
+      'karma-webpack'
     ],
-    captureTimeout: 60000,
+    captureTimeout: 100000,
     singleRun: true
   });
 };
